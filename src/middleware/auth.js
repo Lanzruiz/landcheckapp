b16635cae153bbda0f25acfb77a45df6b044791b
Lanz Ruiz <lanz.primeitph@gmail.com>
2026-05-05 13:57:33 +0800

    fix the verification of the jwt

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,37 @@
 
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
-const SECRET = process.env.JWT_SECRET || "supersecretkey";
+// Standardized response function
+
+const handleResponse = (res, status, message, data = null ) => {
+    res.status(status).json({
+        status, 
+        message, 
+        data,
+    });
+};
+
+const SECRET = process.env.JWT_SECRET;
 
 const auth = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  // Get token from header
+   const token = req.header('x-auth-token')
 
-  if (!token) return res.status(401).json({msg: "No token, authorization denied"});
-
-  //Verify token
+   // Check if not token
+   if(!token) {
+       return handleResponse(res, 401, "No token, authorization denied", null);
+   }
+   //Verify token
     try {
        const decode = jwt.verify(token, SECRET)
 
        req.user = decode.user;
        next();
     } catch(err) {
-       res.status(401).json({ msg: 'Token is not valid'});
+       return handleResponse(res, 401, "Token is not valid", null);
     }
 }
 
--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createProperty, deleteProperty, getAllProperty, getPropertyById, updateProperty } from "../controller/propertyController.js";
+import auth from "../middleware/auth.js";
 
 const propertyRouter = express.Router();
 
-propertyRouter.get("/properties", getAllProperty);
-propertyRouter.get("/property/:id", getPropertyById);
-propertyRouter.post("/property", createProperty);
-propertyRouter.put("/property/:id", updateProperty)
-propertyRouter.delete("/property/:id", deleteProperty);
+propertyRouter.get("/properties", auth, getAllProperty);
+propertyRouter.get("/property/:id", auth, getPropertyById);
+propertyRouter.post("/property", auth, createProperty);
+propertyRouter.put("/property/:id", auth, updateProperty)
+propertyRouter.delete("/property/:id", auth, deleteProperty);
 
 export default propertyRouter;
